e06641e0cf1bc4f65e5c57dd9ef50db22ff5ee3e
Romeo Gouebi <romeoange25@gmail.com>
2020-03-26 13:00:49 +0000

    add text to help

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -1,1 +1,2 @@
 <h1>help</h1>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo odit, expedita est aspernatur accusamus sit dolorem et illo distinctio corporis, neque, error ipsa, eum? Suscipit fugiat totam voluptatum necessitatibus ex.
