42c4b0586316fd53b2398ca73806a59bc85a0eda
Romeo Gouebi <romeoange25@gmail.com>
2020-03-26 13:02:08 +0000

    add text to helpp

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -1,1 +1,13 @@
 <h1>help</h1>
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero neque placeat fugit quis ipsa vitae, blanditiis totam ipsum, tempora, laborum distinctio, similique cum molestiae dignissimos aperiam at! Vero, impedit, reiciendis?
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
